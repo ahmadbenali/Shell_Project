7ab84b8cc5f4fc956495a0b22bf9bfa81211299f
“ahmadbenali” <a7madbenali@gmail.com>
2026-01-25 15:44:27 +0300

    PATH DOne

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
+import java.io.File;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+
+
+    private static final Set<String> Shell_BuiltIn = Set.of("type","exit","echo");
+
+    private static String getPath(String inputPath)
+    {
+        //path is  Env variable, your program will ask os to check every file inside PATH
+        //specifying a set of directories where executable programs are located.
+        String pathEnv=System.getenv("PATH");
+
+        if(pathEnv==null) return null;
+
+        // because the input path will be "/usr/local/bin:/usr/bin:/bin:/usr/games"
+        String []directories=pathEnv.split(":");
+
+        for(String dir:directories)
+        {
+            //It doesn't actually create a file on your hard drive yet;
+            // it simply creates a Java object that represents a potential file location.
+            //Java intelligently combines these into a single path,
+            //handling the slashes for you so you don't have to worry if dir ends with a / or not.
+            File f=new File(dir,inputPath);
+            if(f.exists() && f.canExecute())
+                return f.getAbsolutePath();
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws Exception {
 
 
@@ -23,24 +53,18 @@ public class Main {
                 }
                 case "type" ->{
                     if(parts.length>1) {
-                        if ("exit".equals(parts[1]) || "echo".equals(parts[1]) || "type".equals(parts[1]))
+                        if (Shell_BuiltIn.contains(parts[1]))
                             System.out.println(parts[1] + " is a shell builtin");
-                        else System.out.println(parts[1]+ ": not found");
+                        else
+                        {
+                            String path=getPath(parts[1]);
+                            if(path !=null) System.out.println(parts[1]+" is "+path);
+                            else System.out.println(parts[1]+" not found");
+                        }
                     }
                 }
                 default -> System.out.println(command+": command not found");
             }
-//            if("exit".equals(command)) {
-//                System.exit(0);
-//                //flag=false;}
-//            }
-//
-//            else if("echo".equals(command))
-//            {
-//                System.out.println();
-//            }
-//
-//            else System.out.println(command+": command not found");
 
 
         }
